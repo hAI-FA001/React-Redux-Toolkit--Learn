49219ba4c9c254cdea9ebda14f0c39cd69d2d68e
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-12 17:57:05 +0500

    call getCartItems in App + isLoading conditional rendering + set default cartItems to []

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -2,22 +2,34 @@ import { useEffect } from "react";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateTotal } from "./features/cart/cartSlice";
+import { calculateTotal, getCartItems } from "./features/cart/cartSlice";
 import Modal from "./components/Modal";
 
 function App() {
   const dispatch = useDispatch()
 
-  const { cartItems } = useSelector((state) => state.cart)
+  const { cartItems, isLoading } = useSelector((state) => state.cart)
   const { isOpen } = useSelector((state) => state.modal)
   
   useEffect(() => {
     dispatch(calculateTotal())
   }, [cartItems])
+
+  useEffect(() => {
+    dispatch(getCartItems())
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
   
   return <main>
     {isOpen && <Modal />}
-    
+
     <Navbar />
     <CartContainer />
   </main>;
