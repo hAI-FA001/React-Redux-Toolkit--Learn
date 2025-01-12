9cfcff6a0d372d20c48a8d19879e3864edd1c56c
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-12 15:51:59 +0500

    navbar

---
+++ vite-starter/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { CartIcon } from '../icons'
+
+const Navbar = () => {
+  return (
+      <nav>
+          <div className='nav-center'>
+              <h3>redux toolkit</h3>
+              <div className='nav-container'>
+                  <CartIcon />
+                  <div className='amount-container'>
+                      <p className='total-amount'>Amount</p>
+                  </div>
+              </div>
+          </div>
+    </nav>
+  )
+}
+
+export default Navbar
