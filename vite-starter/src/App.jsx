d35986735787ef2468595dd5312bc88a95e3fc9d
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-12 16:57:55 +0500

    setup Modal compo

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -3,6 +3,7 @@ import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 import { useDispatch, useSelector } from "react-redux";
 import { calculateTotal } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
   const dispatch = useDispatch()
@@ -13,6 +14,7 @@ function App() {
   }, [cartItems])
   
   return <main>
+    <Modal />
     <Navbar />
     <CartContainer />
   </main>;
