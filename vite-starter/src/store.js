7b72f63b46f7c94f2ff4f59284e30521316b4965
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-12 15:47:48 +0500

    cart slice + add in store

--- vite-starter/src/store.js
+++ vite-starter/src/store.js
@@ -1,3 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice'
 
-export const store = configureStore({ reducer: {} });
+export const store = configureStore({ reducer: {cart: cartReducer} });
